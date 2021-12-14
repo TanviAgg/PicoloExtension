d64183134be6b8f13fbbc575aaa6a86e751a04a4
Tanvi Aggarwal <tanviaggarwal@Tanvis-MacBook-Air.local>
2021-12-13 22:14:46 -0500

    Copy the OCR result to clipboard

--- basic_server.py
+++ basic_server.py
@@ -2,6 +2,7 @@ import base64
 
 from flask import Flask, request, make_response
 from flask_cors import CORS, cross_origin
+from extract import extract_text
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -11,13 +12,16 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/', methods=['GET', 'POST'])
 @cross_origin()
 def index():
-	print(request.json)
+	# print(request.json)
 	with open("./examples/imageToSave.png", "wb") as fh:
 		fh.write(base64.decodebytes((request.json['body'][22:]).encode('utf-8')))
+	v = extract_text(None, None, "./examples/imageToSave.png")
+	v = " ".join(v.split("\n")).strip()
+	print(v)
 	response = make_response()
 	response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
 	# response.headers['Access-Control-Allow-Origin'] = '*'
-	response.data = '{"data":"PASSED VALUE"}'
+	response.data = f'"{v}"'
 	return response
 
 
