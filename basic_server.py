7c47a4f8db4b00d57d75960f935ab96537ebf1a9
Tanvi Aggarwal <tanviaggarwal@Tanvis-MacBook-Air.local>
2021-12-15 22:16:09 -0500

    Clean up server script

--- basic_server.py
+++ basic_server.py
@@ -12,7 +12,6 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/', methods=['GET', 'POST'])
 @cross_origin()
 def index():
-	# print(request.json)
 	with open("./examples/imageToSave.png", "wb") as fh:
 		fh.write(base64.decodebytes((request.json['body'][22:]).encode('utf-8')))
 	v = extract_text(None, None, "./examples/imageToSave.png")
@@ -20,16 +19,5 @@ def index():
 	print(v)
 	response = make_response()
 	response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-	# response.headers['Access-Control-Allow-Origin'] = '*'
 	response.data = f'"{v}"'
-	return response
-
-
-@app.route('/ocr', methods=['GET', 'POST'])
-@cross_origin()
-def say_hello():
-	if request.method == 'POST':
-		pass
-	response = make_response()
-	response.headers['Access-Control-Allow-Origin'] = '*'
 	return response
